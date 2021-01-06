0117999c3603e769a43ac5becc04698dc8988cd4
James Jenkinson <j.a.jenkinson@gmx.com>
2021-01-06 15:18:18 +0100

    Add css loader

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import reactDom from 'react-dom'
-
-const App: React.FC = () => (
-  <>
-    Hello world
-  </>
-)
+import App from './App'
 
 reactDom.hydrate(
   <App />,
